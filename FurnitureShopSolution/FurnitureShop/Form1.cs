104fd1e2426e604340ee2c36470f53e4a80e7fd3
DESKTOP-K257G76\coa <coa2102001@gmail.com>
2022-01-10 15:02:37 -0800

    Added filter by product name, color or description on Items

--- FurnitureShopSolution/FurnitureShop/Form1.cs
+++ FurnitureShopSolution/FurnitureShop/Form1.cs
@@ -16,6 +16,8 @@ namespace FurnitureShop
     {
         private readonly ItemBusiness itemBusiness = new ItemBusiness();
         private readonly EmployeeBusiness employeeBusiness = new EmployeeBusiness();
+        BindingSource bs = new BindingSource(); 
+        DataTable dt = new DataTable();
         BindingList<Employee> emp;
         public Form1(string User)
         {
@@ -33,18 +35,32 @@ namespace FurnitureShop
             emp = new BindingList<Employee>(employeeBusiness.GetAllEmployees());
             dataGridEmployees.DataSource = emp;
 
+            dt.Columns.Add("ProductName", typeof(string));
+            dt.Columns.Add("ProductPrice", typeof(decimal));
+            dt.Columns.Add("ProductColor", typeof(string));
+            dt.Columns.Add("ProductDescription", typeof(string));
+            dt.Columns.Add("ProductType", typeof(string));
+
+            dt.Columns.Add("Category", typeof(string));
+            dt.Columns.Add("Stock", typeof(int));
+            dt.Columns.Add("Discount", typeof(int));
 
             dataGridStock.DataSource = itemBusiness.GetInStockItems();
             listBoxItems.HorizontalScrollbar = true;
             foreach(var item in items)
               {
+                dt.Rows.Add(new object[] { item.ProductName, item.ProductPrice, item.ProductColor, item.ProductDescription, item.Type, item.Category, item.Stock, item.Discount });
+
                 listBoxItems.Items.Add(string.Format("{0} / Price:{1} / {2} / {3} / {4} / {5} / In stock:{6} / Discount:{7} ",item.ProductName,item.ProductPrice,item.ProductColor,item.ProductDescription,item.Type,item.Category, item.Stock, item.Discount));
               }
-
+            bs.DataSource = dt;
+            dataGridStock.DataSource = bs;
         }
 
         private void buttonSearch_Click(object sender, EventArgs e)
         {
+            bs.Filter =
+                string.Format("ProductName LIKE '%{0}%' OR ProductDescription LIKE '%{0}%' OR ProductColor LIKE '%{0}%'", textBoxSearch.Text);
 
         }
 
